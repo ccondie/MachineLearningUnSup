34690ddd5f011fddb10a79a100126a17f72217a7
ccondie <clayton.condie@gmail.com>
2017-04-05 22:47:57 -0600

    there be a bug ... but I be too tired to find it

--- toolkit/kmeans.py
+++ toolkit/kmeans.py
@@ -3,7 +3,9 @@ from random import randint
 from random import uniform
 from mpl_toolkits.mplot3d import Axes3D
 
+import operator
 import math
+import sys
 import time
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,11 +15,8 @@ class Centroid(object):
     colors = ['r', 'b', 'g', 'y', 'c', 'm', 'b']
     color_count = 0
 
-    def __init__(self, col_type, col_min, col_max):
-        self.last_delta = 0
-        self.iterations_without_delta = 0
-        self.delta_threshold = 0.1
-
+    def __init__(self, col_type, col_min, col_max, row=None):
+        self.cent_num = Centroid.color_count
         self.color = Centroid.colors[Centroid.color_count]
         Centroid.color_count += 1
 
@@ -28,20 +27,29 @@ class Centroid(object):
 
         # cols will store the "location" of the centroid
         self.location = []
-        for i in range(len(col_type)):
-            if col_type[i] == 'continuous':
-                self.location.append(uniform(col_min[i], col_max[i]))
-            else:
-                self.location.append(randint(math.floor(col_min[i]), math.ceil(col_max)))
+        if row is None:
+            for i in range(len(col_type)):
+                if col_type[i] == 'continuous':
+                    self.location.append(uniform(col_min[i], col_max[i]))
+                else:
+                    self.location.append(randint(math.floor(col_min[i]), math.ceil(col_max[i])))
+        else:
+            self.location = row
 
         # stores the data points connected with this centroid
         self.rows = list()
+
+        print('Initializing Centroid ' + str(self.cent_num) + ': ', end='')
+        for el in self.location:
+            print(el, end=', ')
+        print()
 
     def add_row(self, instance):
         self.rows.append(instance)
 
     def clear_instances(self):
         self.rows = []
+        pass
 
     def update_mean(self):
         """
@@ -49,32 +57,137 @@ class Centroid(object):
         :return:
         """
         for col_index in range(len(self.location)):
-            col_sum = 0
-            for row in self.rows:
-                col_sum += row[col_index]
-            if len(self.rows) == 0:
-                if self.col_type[col_index] == 'continuous':
-                    col_mean = uniform(self.col_min[col_index], self.col_max[col_index])
+            new_col_val = 0
+            # for each location feature
+            if self.col_type[col_index] == 'continuous':
+                # column is continuous
+                col_sum = 0
+                if len(self.rows) == 0:
+                    # if during the assignment phase this centroid had no instances, randomly assign it a new location
+                    new_col_val = uniform(self.col_min[col_index], self.col_max[col_index])
                 else:
-                    pass
+                    cont_val_count = 0
+                    for row in self.rows:
+                        # sum the values for that feature for each of the instances
+                        if row[col_index] != float('Inf'):
+                            col_sum += row[col_index]
+                            cont_val_count += 1
+                    if cont_val_count > 0:
+                        new_col_val = col_sum / cont_val_count
+                    else:
+                        new_col_val = float('Inf')
             else:
-                col_mean = col_sum / len(self.rows)
-            self.last_delta = abs(self.location[col_index] - col_mean)
-            self.location[col_index] = col_mean
-        if self.last_delta < self.delta_threshold:
-            self.iterations_without_delta += 1
-        else:
-            self.iterations_without_delta = 0
+                # column is nominal
+                if len(self.rows) == 0:
+                    new_col_val = float('Inf')
+                else:
+                    vote_map = dict()
+                    for row in self.rows:
+                        col_val = row[col_index]
+                        if col_val == float('Inf'):
+                            continue
+
+                        if col_val in vote_map:
+                            vote_map[col_val] += 1
+                        else:
+                            vote_map[col_val] = 1
+
+                    if len(vote_map) == 0:
+                        new_col_val = float('Inf')
+                    else:
+                        new_col_val = self.tie_break(vote_map)
+
+            self.location[col_index] = new_col_val
+
+    def tie_break(self, votes):
+        highest_vote = 0
+        lowest_feature_id = sys.maxsize
+
+        for id in votes:
+            count = votes[id]
+            if count > highest_vote:
+                highest_vote = count
+                lowest_feature_id = id
+            elif count == highest_vote:
+                if id < lowest_feature_id:
+                    highest_vote = count
+                    lowest_feature_id = id
+        return lowest_feature_id
 
     def print(self):
-        print(self.location)
+        print('CENTROID ' + str(self.cent_num) + ' VALUES: ', end='')
+        self.print_location()
+        print('CENTROID INSTANCE COUNT: ' + str(len(self.rows)))
+        print('SSE of CLUSTER: ' + str(self.sse()))
+        print()
+
+    def print_location(self):
+        for col_index in range(len(self.location)):
+            if self.col_type[col_index] == 'nominal':
+                if col_index == len(self.location)-1:
+                    print(KMeansLearner.bad_code_practice.attr_value(col_index, self.location[col_index]), end='')
+                else:
+                    print(KMeansLearner.bad_code_practice.attr_value(col_index, self.location[col_index]), end=',\t')
+            else:
+                if col_index == len(self.location) - 1:
+                    print("{:0.3f}".format(self.location[col_index]), end='')
+                else:
+                    print("{:0.3f}".format(self.location[col_index]), end=',\t')
+        print()
+
+    def sse(self):
+        running_sum = 0
+        for row in self.rows:
+            running_sum += distance(row, self) ** 2
+        return running_sum
+
+
+def distance(row, centroid):
+    distance_sum = 0
+    for col_index in range(len(row)):
+        row_el = row[col_index]
+        cent_el = centroid.location[col_index]
+
+        if row_el == float('Inf') or cent_el == float('Inf'):
+            distance_sum += 1
+            continue
+
+        if centroid.col_type[col_index] == 'nominal':
+            if row_el != cent_el:
+                distance_sum += 1
+            continue
+
+        distance_sum += (cent_el - row_el) ** 2
+
+    return math.sqrt(distance_sum)
+
+# def distance(row, centroid):
+#     distance_sum = 0
+#     for col_index in range(len(row)):
+#         row_el = row[col_index]
+#         cent_el = centroid.location[col_index]
+#
+#         if row_el == float('Inf') or cent_el == float('Inf'):
+#             distance_sum += 1
+#             continue
+#
+#         if centroid.col_type[col_index] == 'nominal':
+#             if row_el != cent_el:
+#                 distance_sum += 1
+#             continue
+#
+#         distance_sum += (cent_el - row_el) ** 2
+#
+#     return math.sqrt(distance_sum)
 
 
 class KMeansLearner(object):
     count = 0
+    bad_code_practice = None
 
     def __init__(self):
         self.k = 5
+        self.random_start = False
         self.centroids = []
         self.color_count = 0
 
@@ -83,10 +196,9 @@ class KMeansLearner(object):
         :param rows:
         :return:
         """
-
         settle_count = 5
-
-        plt.ion()
+        carry_over_sse = 100
+        KMeansLearner.bad_code_practice = rows
 
         # calculate the data 'type' of each col
         col_type = []
@@ -102,61 +214,70 @@ class KMeansLearner(object):
             col_max.append(rows.column_max(col_index))
 
         # initialize the centroids
-        for k in range(self.k):
-            self.centroids.append(Centroid(col_type, col_min, col_max))
+        if self.random_start:
+            for k in range(self.k):
+                self.centroids.append(Centroid(col_type, col_min, col_max))
+        else:
+            for k in range(self.k):
+                self.centroids.append(Centroid(col_type, col_min, col_max, rows.row(k)))
 
-        plt.show()
         done = False
+        iteration_count = 0
+
         while not done:
+            iteration_count += 1
             done = True
+
+            print('*************************************')
+            print('Iteration ' + str(iteration_count))
+            print('*************************************')
+            print('Making Assignments')
             # assign each row to a centroid
             for row_index in range(rows.rows):
+                # print('\t' + str(row_index) + '=', end='')
+
                 row = rows.row(row_index)
                 nearest_centroid = 0
-                nearest_distance = self.distance(row, self.centroids[0])
+                nearest_distance = distance(row, self.centroids[0])
                 for centroid_index in range(1, len(self.centroids)):
-                    this_distance = self.distance(row, self.centroids[centroid_index])
+                    this_distance = distance(row, self.centroids[centroid_index])
                     if this_distance < nearest_distance:
                         nearest_distance = this_distance
                         nearest_centroid = centroid_index
                 self.centroids[nearest_centroid].add_row(row)
-            self.plot_2d_centroids()
 
-            # realign the centroids
+                print(nearest_centroid, end='')
+                # if (row_index + 1) % 10 == 0:
+                #     print('\n', end='')
+            print()
+
+            total_sse = 0
             for centroid in self.centroids:
-                centroid.update_mean()
-            self.plot_2d_centroids()
+                total_sse += centroid.sse()
+            print('SSE: ' + "{:0.3f}".format(total_sse))
 
-            for centroid in self.centroids:
-                centroid.clear_instances()
+            # check if the escape parameters have been met
+            # if abs(total_sse - carry_over_sse) > 5:
+            #     done = False
+            done = False
 
-            for centroid in self.centroids:
-                if centroid.iterations_without_delta < settle_count:
-                    done = False
+            if not done:
+                # if we have yet to reach a stable configuration, realign the cluster and try again
+                print('Computing Centroids:')
+                # realign the centroids
+                for centroid in self.centroids:
+                    centroid.update_mean()
+                    print('\tCentroid ' + str(centroid.cent_num) + ' = ', end='')
+                    centroid.print_location()
 
-        plt.show()
-        print('Done ... you have 30 seconds')
-        time.sleep(30)
+                for centroid in self.centroids:
+                    centroid.clear_instances()
 
-    def classify(self):
-        """
-        :return:
-        """
+            time.sleep(5)
 
-        """
-        find the closest centroid
-        """
-        pass
-
-    def distance(self, row, centroid):
-        distance = 0
-        for col_index in range(len(row)):
-            if centroid.col_type[col_index] == 'continuous':
-                distance += (row[col_index] - centroid.location[col_index]) ** 2
-            else:
-                if centroid.location[col_index] != row[col_index]:
-                    distance += 1
-        return math.sqrt(distance)
+        for centroid in self.centroids:
+            # output clustering information
+            centroid.print()
 
     def plot_2d_centroids(self):
         plt.clf()
@@ -169,11 +290,11 @@ class KMeansLearner(object):
                 x.append(row[0])
                 y.append(row[1])
                 colors.append(centroid.color)
-                area.append(np.pi * 3 ** 2)
+                area.append(np.pi * 2 ** 2)
             # plot the center of the centroid
             x.append(centroid.location[0])
             y.append(centroid.location[1])
             colors.append(centroid.color)
-            area.append(np.pi * 10 ** 2)
+            area.append(np.pi * 6 ** 2)
         plt.scatter(x, y, s=area, c=colors, alpha=1.0)
         plt.pause(0.05)
